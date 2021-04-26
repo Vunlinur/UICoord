9d523be5b0fbfe4e929162dd3203ddda1ecf1573
Linur <Adam.Porecki@lionbridge.com>
2021-04-26 02:28:17 +0200

    Implement export to clipboard MVP

--- Menu.py
+++ Menu.py
@@ -1,50 +1,62 @@
+from os import linesep
 from tkinter import *
 from tkinter import ttk
 
 
 class Menu(Frame):
     def __init__(self, parent: Tk, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
+        Frame.__init__(self, parent, width=400, *args, **kwargs)
         self.parent = parent
 
         #  Member variables
         self.columns = ('Name', 'x', 'y', 'x2', 'y2')
         self.last_focus = None
         self.coord_text = StringVar()
+        self.update_coords_label(0, 0)
+        self.pattern = StringVar()
+        self.pattern.set('"{Name}": ({x}, {y}, {x2}, {y2}),')
 
         #  Setup UI
-        self.menu = Frame(self.parent, width=400)
-        self.menu.pack(side=LEFT, fill=Y, expand=False)
-        self.menu.pack_propagate(0)
+        self.pack(side=LEFT, fill=Y, expand=False)
+        self.pack_propagate(0)
 
-        self.label = Label(self.menu, textvariable=self.coord_text, font=18)
+        self.label = Label(self, textvariable=self.coord_text, font=18)
         self.label.pack()
 
-
+        # tree
         self.tree = ttk.Treeview(columns=self.columns, show='headings')
         scrollbar_y = ttk.Scrollbar(orient=VERTICAL, command=self.tree.yview)
         scrollbar_x = ttk.Scrollbar(orient=HORIZONTAL, command=self.tree.xview)
         self.tree['yscroll'] = scrollbar_y.set
         self.tree['xscroll'] = scrollbar_x.set
 
+        for column in self.columns:
+            self.tree.heading(column, text=column)
+            width = 160 if column == "Name" else 60
+            self.tree.column(column, minwidth=32, width=width, stretch=NO)
+
         # add tree and scrollbars to frame
-        self.coords = Frame(self.menu)
+        self.coords = Frame(self)
         self.tree.grid(in_=self.coords, row=0, column=0, sticky=NSEW)
         scrollbar_y.grid(in_=self.coords, row=0, column=1, sticky=NS)
         scrollbar_x.grid(in_=self.coords, row=1, column=0, sticky=EW)
 
         self.coords.grid_rowconfigure(0, weight=1)
         self.coords.grid_columnconfigure(0, weight=1)
-        self.coords.pack(side=BOTTOM, fill=Y, expand=YES, padx=12, pady=12)
+        self.coords.pack(fill=Y, expand=YES, padx=12, pady=12)
 
-        for column in self.columns:
-            self.tree.heading(column, text=column)
-            width = 160 if column == "Name" else 60
-            self.tree.column(column, minwidth=32, width=width, stretch=NO)
+        # export
+        self.export = Frame(self)
+        self.export.pack(side=BOTTOM, fill=X, padx=12, pady=12)
+
+        self.pattern_entry = Entry(self.export, textvariable=self.pattern)
+        self.pattern_entry.pack(side=LEFT, fill=X, expand=Y)
+
+        self.export_button = Button(self.export, text="Copy to Clipboard", command=self.format_and_copy)
+        self.export_button.pack(side=RIGHT)
 
         # Binds, callbacks
         self.tree.bind("<Motion>", self.on_list_mouseover)
-        self.tree.bind('<Button-1>', self.select_item)
 
         self.paint_marker_from_list_callback = None
 
@@ -61,9 +73,17 @@ class Menu(Frame):
 
             self.paint_marker_from_list_callback(values)
 
-    def select_item(self, event):
-        curItem = self.tree.focus()
-        print(self.tree.item(curItem))
+    def format_and_copy(self):
+        def format_line(item_id):
+            # Sure, this could be a comprehension. Good luck reading it though.
+            values = self.tree.item(item_id)["values"]
+            coord = {key: values[i] if i < len(values) else values[i - 2] for i, key in
+                     enumerate(self.columns)}  # "-2" is a very flimsy solution. needs moving to MVC.
+            return self.pattern.get().format(**coord)
+
+        rows = linesep.join([format_line(row) for row in self.tree.get_children()])
+        self.parent.clipboard_clear()
+        self.parent.clipboard_append(rows)
 
     def update_coords_label(self, x, y):
         self.coord_text.set('x:{:.4f}, y:{:.4f}'.format(x, y))
--- Workspace.py
+++ Workspace.py
@@ -5,14 +5,13 @@ from tkinter import simpledialog
 from PIL import ImageTk, Image
 
 
-class Workspace(Frame):
+class Workspace(Canvas):
     def __init__(self, parent: Tk, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
+        Canvas.__init__(self, parent, bd=0, *args, **kwargs)
         self.parent = parent
 
         #  Setup UI
-        self.canvas = Canvas(self.parent, bd=0)
-        self.canvas.pack(side=LEFT, fill=BOTH, expand=YES, padx=12, pady=12)
+        self.pack(side=LEFT, fill=BOTH, expand=YES, padx=12, pady=12)
 
         #  Member variables
         self.image_original = Image.open("uisample.png")
@@ -20,14 +19,14 @@ class Workspace(Frame):
         self.photo_image = None  # set in on_resize
         self.image_aspect_ratio = self.image.size[0] / self.image.size[1]
         self.last_coord = (0.0, 0.0)
-        self.marker = self.canvas.create_rectangle(0, 0, 0, 0)
+        self.marker = self.create_rectangle(0, 0, 0, 0)
 
         # Binds, callbacks
-        self.canvas.bind('<Motion>', self.on_mouseover)
-        self.canvas.bind("<Configure>", self.on_resize)
-        self.canvas.bind("<Button-1>", self.on_click)
-        self.canvas.bind("<Button-3>", self.on_right_click)
-        self.canvas.bind("<ButtonRelease-1>", self.on_click_release)
+        self.bind('<Motion>', self.on_mouseover)
+        self.bind("<Configure>", self.on_resize)
+        self.bind("<Button-1>", self.on_click)
+        self.bind("<Button-3>", self.on_right_click)
+        self.bind("<ButtonRelease-1>", self.on_click_release)
 
         self.menu_insert_callback = None
         self.update_coords_label_callback = None
@@ -42,7 +41,7 @@ class Workspace(Frame):
     def paint_marker_from_workspace(self, event):
         if self.last_coord != (0, 0):
             self.clear_marker()
-            self.marker = self.canvas.create_rectangle(self.last_coord[0], self.last_coord[1], event.x,
+            self.marker = self.create_rectangle(self.last_coord[0], self.last_coord[1], event.x,
                                                        event.y, outline="red", width=2)
 
     def on_resize(self, event):
@@ -54,7 +53,7 @@ class Workspace(Frame):
             scale = (event.width, int(event.width / self.image_aspect_ratio))
         self.image = self.image_original.resize(scale)
         self.photo_image = ImageTk.PhotoImage(self.image)
-        self.canvas.create_image(0, 0, anchor=NW, image=self.photo_image)
+        self.create_image(0, 0, anchor=NW, image=self.photo_image)
 
     def on_click(self, event):
         self.last_coord = (event.x, event.y)
@@ -79,13 +78,15 @@ class Workspace(Frame):
         if distance < 8:
             coord = (name,
                      event.x / self.image.size[0],
-                     event.y / self.image.size[1])
+                     event.y / self.image.size[1]
+            )
         else:
             coord = (name,
                      self.last_coord[0] / self.image.size[0],
                      self.last_coord[1] / self.image.size[1],
                      event.x / self.image.size[0],
-                     event.y / self.image.size[1])
+                     event.y / self.image.size[1]
+            )
 
         self.menu_insert_callback(coord)
         self.clear_marker()
@@ -102,7 +103,7 @@ class Workspace(Frame):
         }
         coords = values[1:]
         if len(coords) == 4:
-            self.marker = self.canvas.create_rectangle(
+            self.marker = self.create_rectangle(
                 float(coords[0]) * self.image.size[0],
                 float(coords[1]) * self.image.size[1],
                 float(coords[2]) * self.image.size[0],
@@ -111,7 +112,7 @@ class Workspace(Frame):
             )
         else:
             radius = 3
-            self.marker = self.canvas.create_oval(
+            self.marker = self.create_oval(
                 float(coords[0]) * self.image.size[0] - radius,
                 float(coords[1]) * self.image.size[1] - radius,
                 float(coords[0]) * self.image.size[0] + radius,
@@ -120,4 +121,4 @@ class Workspace(Frame):
             )
 
     def clear_marker(self):
-        self.canvas.delete(self.marker)
+        self.delete(self.marker)
