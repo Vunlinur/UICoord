ba8448f99fd71a730b8e55ec3cd90d5c65a20df5
Linur <Adam.Porecki@lionbridge.com>
2021-05-08 01:03:57 +0200

    Separate Coord class from Model.py

--- Model.py
+++ Model.py
@@ -3,29 +3,7 @@ from io import BytesIO
 
 from PIL.Image import Image, open as OpenImage
 
-
-class Coord:
-    POINT = "POINT"
-    RECTANGLE = "RECTANGLE"
-    COLUMN_DEFAULTS = {
-        "name": "Undefined",
-        "x1": None,
-        "y1": None,
-        "x2": None,
-        "y2": None,
-    }
-
-    def __init__(self, **kwargs):
-        """
-        Add variables to the object based on their static list.
-        Set the variable to the value from kwargs, if given.
-        Set to default otherwise.
-        """
-        [self.__setattr__(key, kwargs.get(key, val)) for key, val in self.COLUMN_DEFAULTS.items()]
-        self.type = self.POINT if self.x2 == None and self.y2 == None else self.RECTANGLE
-
-    def row_data(self):
-        return [self.__getattribute__(key) or "" for key in self.COLUMN_DEFAULTS.keys()]
+from Coord import Coord
 
 
 class Model:
